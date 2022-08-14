45855cbed6c6b860ac64931b152cc64e126fa5f8
MyRedHeart <108692278+MyRedHeart@users.noreply.github.com>
2022-08-14 21:01:13 +0300

    My first project Go

---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"html/template"
+	"net/http"
+)
+
+type Article struct {
+	Id                     uint16
+	Title, Anons, FullText string
+}
+
+var posts = []Article{}
+var showPost = Article{}
+
+func index(w http.ResponseWriter, r *http.Request) { //срабатывает при переходе на главную(параметр для записи, параметр для получения данных)
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html") //создаем переменную t, взаимодействие с файлом index.html
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	res, err := db.Query("SELECT * FROM articles")
+	if err != nil {
+		panic(err)
+	}
+
+	posts = []Article{}
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf(" Post: %s with ID %d", post.Title, post.Id)
+
+		posts = append(posts, post)
+	}
+
+	t.ExecuteTemplate(w, "index", posts) //метод динамическое подключение(на какую страницу вывод, какой блок выведем, доп. параметры)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/create.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "create", nil)
+}
+
+func save_article(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Не все данные заполнены")
+	} else {
+		db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+		if err != nil {
+			panic(err)
+		}
+
+		defer db.Close()
+
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO articles (title, anons, full_text) VALUES('%s', '%s', '%s')", title, anons, full_text))
+		if err != nil {
+			panic(err)
+		}
+
+		defer insert.Close()
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+func show_post(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	t, err := template.ParseFiles("templates/show.html", "templates/header.html", "templates/footer.html")
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	res, err := db.Query(fmt.Sprintf("SELECT * FROM articles WHERE id = '%s'", vars["id"]))
+	if err != nil {
+		panic(err)
+	}
+
+	showPost = Article{}
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf(" Post: %s with ID %d", post.Title, post.Id)
+
+		showPost = post
+	}
+
+	t.ExecuteTemplate(w, "show", showPost)
+}
+
+func handleFunc() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/", index).Methods("GET") //отслеживание главной страницы
+	rtr.HandleFunc("/create", create).Methods("GET")
+	rtr.HandleFunc("/save_article", save_article).Methods("POST")
+	rtr.HandleFunc("/post/{id:[0-9]+}", show_post).Methods("GET")
+
+	http.Handle("/", rtr)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))) // обрабатываем URL(что удаляем, удаляет префикс(что удаляем, указание откуда удаляем(в папке(название папки))))
+	http.ListenAndServe(":8080", nil)                                                             //запуск локального сервера(порт, настройки)
+}
+
+func main() {
+	handleFunc()
+}
